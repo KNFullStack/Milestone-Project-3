b025c979cc92bc3d84f8632b1d61e3dd5982852c
KNFullStack <knfullstack@gmail.com>
2022-03-07 15:21:43 +0000

    basic placeholders added for landing page, basic login and register forms added - backend TBD

--- app.py
+++ app.py
@@ -33,14 +33,21 @@ def dashboard():
     return render_template("dashboard.html", income=income, outgoings=outgoings)
 
 
+@app.route("/login", methods=["GET","POST"])
+def login():
+    return render_template("login.html")
+
+
 @app.route("/register", methods=["GET","POST"])
 def register():
     return render_template("register.html")
 
 
-@app.route("/login", methods=["GET","POST"])
-def login():
-    return render_template("login.html")
+@app.route("/logout")
+def logout():
+    # do xyz
+    # do flash
+    return redirect(url_for('login'))
 
 
 if __name__ == "__main__":
