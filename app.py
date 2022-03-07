0db2a745166861dcc7f123ecfbecd931123210de
KNFullStack <knfullstack@gmail.com>
2022-03-07 22:46:28 +0000

    added functionality to filter records returned based on logged in user, hid links depending on logged in status

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta
+
+
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+app.permanent_session_lifetime = timedelta(days=1)
 
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -19,7 +23,6 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-# tests = mongo.db.testing.find() - this is how we get our records from testing collection
 
 @app.route("/")
 def home():
@@ -28,20 +31,31 @@ def home():
 
 @app.route("/dashboard")
 def dashboard():
-    income = mongo.db.income.find()
-    outgoings = mongo.db.outgoings.find()
+    if not "user" in session:
+        flash("Please log in.")
+        return redirect(url_for("login"))
+
+
+    user = session["user"].lower()
+    income = mongo.db.income.find({"created_by": user})
+    outgoings = mongo.db.outgoings.find({"created_by": user})
     return render_template("dashboard.html", income=income, outgoings=outgoings)
 
 
 @app.route("/login", methods=["GET","POST"])
 def login():
+    if "user" in session:
+        flash("You are already logged in.")
+        return redirect(url_for("dashboard"))
+
     if request.method == "POST":
         existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
 
         if existing_user:
             if check_password_hash(existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("You are now logged in, {}".format(request.form.get("username")))
+                session.permanent = True
+                flash("Welcome back, {}.".format(request.form.get("username")))
                 return redirect(url_for("dashboard", username=session["user"]))
 
             else:
@@ -57,6 +71,10 @@ def login():
 
 @app.route("/register", methods=["GET","POST"])
 def register():
+    if "user" in session:
+        flash("You must log out first.")
+        return redirect(url_for("dashboard"))
+    
     if request.method == "POST":
         existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
 
@@ -80,9 +98,14 @@ def register():
 
 @app.route("/logout")
 def logout():
-    # do xyz
-    # do flash
-    return redirect(url_for('login'))
+    if "user" in session:
+        flash("You have been logged out.")
+        session.pop("user")
+        return redirect(url_for('login'))
+    else:
+        flash("You are not logged in.")
+        return redirect(url_for('login'))
+
 
 
 if __name__ == "__main__":
