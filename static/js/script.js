77a8e846c421a1e0396a0a1ce54173b69d09e067
KNFullStack <knfullstack@gmail.com>
2022-03-10 15:33:58 +0000

    confirm password box added with javascript to make register button unclickable if they do not match

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+let passwordOne = document.getElementById("password")
+let passwordTwo = document.getElementById("password_check")
+let registerButton = document.getElementById("register_button")
+
+passwordOne.addEventListener("input", confirmPassword)
+passwordTwo.addEventListener("input", confirmPassword)
+
+function confirmPassword() {
+    if (passwordOne.value != passwordTwo.value) {
+        registerButton.classList.add("unclickable")
+    } else {
+        registerButton.classList.remove("unclickable")
+    }
+}
