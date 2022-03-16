c5ca27119b9c0b84d97169e6640411869c099e00
KNFullStack <knfullstack@gmail.com>
2022-03-16 19:56:08 +0000

    currency javascript code added for local storage component to remember currency last selected.

--- static/js/currencyScript.js
+++ static/js/currencyScript.js
@@ -1,21 +1,53 @@
 // Currency Selector
 let currency = document.getElementById('currency')
 let currencySpan = document.getElementsByClassName("currencyPlacement")
+const storedCurrency = JSON.parse(localStorage.getItem('currency'));
+window.addEventListener("load", () => {
+    currency.value = storedCurrency;
+    loadCurrency()
+})
 
+
+// Function to check if storedCurrency exists, then uses Switch to set currency.
+function loadCurrency() {
+    switch (storedCurrency) {
+        case "GBP":
+            for (let i = 0; i < currencySpan.length; i++) {
+                currencySpan[i].innerText = "£";
+            }
+            break;
+        case "USD":
+            for (let i = 0; i < currencySpan.length; i++) {
+                currencySpan[i].innerText = "$"
+            }
+            break;
+        case "Euro":
+            for (let i = 0; i < currencySpan.length; i++) {
+                currencySpan[i].innerText = "€"
+            }
+            break;
+    }
+}
+
+// Function to change the localStorage currency item based on a selection, then alter currency accordingly.
 currency.addEventListener("change", () => {
     if (currency.value == "GBP") {
+        window.localStorage.setItem("currency", JSON.stringify(currency.value));
         for (let i = 0; i < currencySpan.length; i++) {
             currencySpan[i].innerText = "£"
         }
     } else if (currency.value == "USD") {
+        window.localStorage.setItem("currency", JSON.stringify(currency.value));
         for (let i = 0; i < currencySpan.length; i++) {
             currencySpan[i].innerText = "$"
         }
     } else if (currency.value == "Euro") {
+        window.localStorage.setItem("currency", JSON.stringify(currency.value));
         for (let i = 0; i < currencySpan.length; i++) {
             currencySpan[i].innerText = "€"
         }
     } else {
+        window.localStorage.setItem("currency", JSON.stringify(currency.value));
         for (let i = 0; i < currencySpan.length; i++) {
             currencySpan[i].innerText = ""
         }
